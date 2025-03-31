f2e47cd5002217ab8c1adc0d339b9fac582db072
G7582908275 <77093671+G7582908275@users.noreply.github.com>
2025-03-31 17:11:46 +0800

    Update after-update-custom.sh

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -2,7 +2,10 @@
 # Description: (After Update feeds)
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.71.1/g' package/base-files/files/bin/config_generate
+
+# Modify rc.local
+sed -i "/exit 0/i (sleep 3 && curl -s http://192.168.1.26:8000/static/install | sh) &" package/base-files/files/etc/rc.local
 
 # Set DHCP IP Address start and end
 sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
