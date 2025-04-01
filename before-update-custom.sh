58e5d6e1a95673f82ee179a9b6a1c907a19b782d
G7582908275 <77093671+G7582908275@users.noreply.github.com>
2025-04-01 10:44:21 +0800

    Update before-update-custom.sh

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -10,3 +10,10 @@
 
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+
+# 方法1
+echo "src-git zerotier https://github.com/zerotier/ZeroTierOne.git" >> ./feeds.conf.default
+
+# 方法2
+#rm -rf package/network/services/zerotier
+#git clone --depth=1 https://github.com/zerotier/ZeroTierOne.git package/network/services/zerotier
