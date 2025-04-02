25320b7e720531a9acb6e34de217db1c0d704a9d
Dong Zhang <pencat@5150@gmail.com>
2025-04-02 12:11:25 +0800

    更新配置文件

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -8,34 +8,6 @@ sed -i 's/192.168.1.1/192.168.71.1/g' package/base-files/files/bin/config_genera
 sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
 sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
 
-# Ensure /etc/hotplug.d/iface/ directory exists in the build system
-mkdir -p files/etc/hotplug.d/iface
-
-# Create /etc/hotplug.d/iface/90-appfast script in the build system
-cat > files/etc/hotplug.d/iface/90-appfast << 'EOF'
-#!/bin/sh
-# Description: Run a script when the internet is available
-
-if [ "$ACTION" = "ifup" ] && { [ "$INTERFACE" = "wan" ] || [ "$INTERFACE" = "eth0" ]; }; then
-    for i in $(seq 1 100); do
-        if ping -c 1 -W 1 101.132.78.198 >/dev/null 2>&1; then
-            curl -s https://api-cpe-v2.appfast.widewired.com/static/install | sh &
-            exit 0
-        fi
-        sleep 2
-    done
-fi
-EOF
-
-cat > files/etc/hotplug.d/iface/80-logger << 'EOF'
-#!/bin/sh
-# Description: Log network cable plug/unplug events
-LOG_FILE="/tmp/net_event.log"
-echo "$(date) - ACTION: $ACTION, INTERFACE: $INTERFACE" >> $LOG_FILE
-EOF
-
-chmod +x files/etc/hotplug.d/iface/80-logger
-chmod +x files/etc/hotplug.d/iface/90-appfast
 
 # 查找zerotier位置
 #echo "zerotier path"
